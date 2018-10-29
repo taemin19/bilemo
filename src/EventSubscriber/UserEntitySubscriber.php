30fcb23f7511170f0ad6e15078a177634e100165
taemin19 <daniel.thebault19@gmail.com>
2018-10-29 16:25:28 +0100

    api(event): set client of User automatically
    Create an EventSubscriber:
    - to get the "currentUser" and automatically set it to the client property when adding a user

---
+++ src/EventSubscriber/UserEntitySubscriber.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class UserEntitySubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    /**
+     * UserEntitySubscriber constructor.
+     * @param TokenStorageInterface $tokenStorage
+     */
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['setCurrentClient', EventPriorities::PRE_WRITE],
+        ];
+    }
+
+    public function setCurrentClient(GetResponseForControllerResultEvent $event)
+    {
+        $user = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if (!$user instanceof User || Request::METHOD_POST !== $method) {
+            return;
+        }
+
+        $user->setClient($this->tokenStorage->getToken()->getUser());
+    }
+}
