24ff9f4a5c48e3539d83e804f4babda6afde7c72
taemin19 <daniel.thebault19@gmail.com>
2018-11-09 17:36:17 +0100

    api(cache): enable the Symfony reverse proxy

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\CacheKernel;
 use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
@@ -33,6 +34,12 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
 }
 
 $kernel = new Kernel($env, $debug);
+
+// Wrap the default Kernel with the CacheKernel one in 'prod' environment
+if ('prod' === $env) {
+    $kernel = new CacheKernel($kernel);
+}
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
